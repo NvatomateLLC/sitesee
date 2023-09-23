4066754bf10144ca28037b95c1a34e1803c7a80b
nickharmon <nick@blockchainglobalfund.com>
2023-09-23 18:49:24 -0400

    added templates, edited app.py

--- app.py
+++ app.py
@@ -1,47 +1,45 @@
 from flask import Flask, render_template
 import requests
+import json
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
 app = Flask(__name__)
 
-# Define the SmartPiXL API endpoint
-API_ENDPOINT = "https://api.smartpixl.com/data"  # Replace with the actual API endpoint
+# Define the base URL and UserTokenID
+base_url = "https://smartpixl.tech/Api2.0/CreatePiXL"
+user_token_id = "XGnkB6g16STzF20ujIEB6kEd67h9y"
 
-# Define the mandatory parameters for the API request
-params = {
-    "CompanyId": "12493",  # Replace with the actual Company ID number
-    "PiXLId": "1",  # Replace with the actual PiXL ID number
-    "Startdate": "2023-01-01",  # Replace with the actual start date
-    "Enddate": "2023-09-30",  # Replace with the actual end date
-    "UserTokenId": "w3mEBSaqhNiHj5XhKhzS4JhhVcbyJIqWAdw"  # Replace with the actual User Token ID
+# Define the model data (replace with actual model data)
+model_data = {
+    # Add the actual key-value pairs of the model data
 }
 
 @app.route('/')
 def home():
-    # Make an API request to the SmartPiXL API
-    response = requests.get(API_ENDPOINT, params=params)
+    # Construct the full URL
+    url = f"{base_url}/{user_token_id}"
+    
+    # Convert model data to JSON
+    json_data = json.dumps(model_data)
+    
+    # Set the headers for the request
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    # Make a POST request to the API
+    response = requests.post(url, data=json_data, headers=headers)
     
     # Check if the request was successful
     if response.status_code == 200:
-        data = response.json()  # Parse the JSON response
-        
-        # Visualize the data (adjust according to the actual data structure)
-        plt.figure(figsize=(10,6))
-        # Example: plt.plot(data['x'], data['y'])
-        plt.title('SmartPiXL Data Visualization')
-        
-        # Encode the plot to PNG
-        img = BytesIO()
-        plt.savefig(img, format='png')
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode()
-        
-        return render_template('home.html', plot_url=plot_url)
+        result = response.json()
+        # Process the result and render the appropriate template
+        return render_template('success.html', result=result)
     else:
-        # Handle API request failure
-        return render_template('error.html', error_message="Failed to retrieve data from SmartPiXL API.")
+        # Handle API request failure and render an error template
+        return render_template('error.html', error_message="Failed to make API request.")
 
 if __name__ == '__main__':
     app.run(debug=True)
