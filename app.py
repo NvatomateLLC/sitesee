de768a4edb7495ccfe66c040bdea24e10b6bc98e
nickharmon <nick@blockchainglobalfund.com>
2023-09-23 17:47:12 -0400

    updated app.py for API calls, edit API_URL

--- app.py
+++ app.py
@@ -8,7 +8,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    # Make an API request
+    response = requests.get('API_URL')  # Replace 'API_URL' with the actual URL of the API you are using
+    data = response.json()  # Assume the response is in JSON format
+    
+    # Visualize the data
+    plt.figure(figsize=(10,6))
+    # Adjust the plotting according to the structure of the API data
+    plt.plot(data['x'], data['y'])  # Assume the data has 'x' and 'y' values
+    plt.title('API Data Visualization')
+    
+    # Encode the plot to PNG
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    
+    return render_template('home.html', plot_url=plot_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
